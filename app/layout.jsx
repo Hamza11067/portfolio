02c66243e2a070626d8bb764daec11b72f456f9d
Hamza11067 <hamzakhalid1067@gmail.com>
2026-05-06 15:35:05 +0500

    Trying to get the font jetBrainsMono

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const JetBrainsMono = JetBrains_Mono({
+  variable: "--font-jetBrainsMono",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"]
 });
 
 export const metadata = {
@@ -20,9 +16,9 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${JetBrainsMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col font-mono">{children}</body>
     </html>
   );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>Hamza Khalid</main>
+    <main className="text-3xl">Hamza Khalid</main>
   );
 }
